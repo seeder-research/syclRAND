fb63028b341dca4fe11e7c6e30d753fb9a2e5be4
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2020-10-10 18:34:43 +0800

    Added mrg63k3a prng

--- src/generators/mrg63k3a.hpp
+++ src/generators/mrg63k3a.hpp
@@ -1,8 +1,10 @@
-typedef struct{
-        long s10, s11, s12, s20, s21, s22;
-} mrg63k3a_state;
+#ifndef __MRG63K3A_RNG__
+#define __MRG63K3A_RNG__
 
-const char * mrg63k3a_prng_kernel = R"EOK(
+#ifndef __SYCLRAND_BASE_CLASS
+#include "common/syclrand_def.hpp"
+#endif // __SYCLRAND_BASE_CLASS
+
 /**
 @file
 
@@ -131,24 +133,138 @@ Generates a random 32-bit unsigned integer using mrg63k3a RNG.
 */
 #define mrg63k3a_uint(state) ((uint)mrg63k3a_ulong(state))
 
-/**
-Generates a random float using mrg63k3a RNG.
+// Kernel function
+// Seed RNG by single ulong
+class mrg63k3a_seed_by_value_kernel {
+	public:
+	    using state_accessor =
+		    sycl::accessor<mrg63k3a_state, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+		mrg63k3a_seed_by_value_kernel(ulong val,
+			state_accessor statePtr)
+		: seedVal(val),
+		  stateBuf(statePtr) {}
+		void operator()(sycl::nd_item<1> item) {
+            uint gid=get_global_linear_id(0);
+            ulong seed = (ulong)(gid);
+            seed <<= 1;
+            seed += seedVal;
+            if (seed == 0) {
+                seed += 1;
+            }
+            mrg63k3a_state state;
+            mrg63k3a_seed(&state, seed);
+            stateBuf[gid] = state;
+		}
 
-@param state State of the RNG to use.
-*/
-#define mrg63k3a_float(state) (mrg63k3a_ulong(state)*MRG63K3A_FLOAT_MULTI)
+	private:
+		ulong           seedVal;
+		state_accessor  stateBuf;
+};
 
-/**
-Generates a random double using mrg63k3a RNG.
+// Kernel function
+// Seed RNG by array of ulong
+class mrg63k3a_seed_by_array_kernel {
+	public:
+	    using state_accessor =
+		    sycl::accessor<mrg63k3a_state, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+	    using input_accessor =
+		    sycl::accessor<ulong, 1, sycl::access::mode::read, sycl::access::target::global_buffer>;
+		mrg63k3a_seed_by_array_kernel(input_accessor seedArr,
+			state_accessor statePtr)
+		: seedArr(seedArr),
+		  stateBuf(statePtr) {}
+		void operator()(sycl::nd_item<1> item) {
+            uint gid=get_global_id(0);
+            ulong seed = seedArr[gid];
+            mrg63k3a_state state;
+            mrg63k3a_seed(&state,seed);
+            stateBuf[gid] = state;
+		}
 
-@param state State of the RNG to use.
-*/
-#define mrg63k3a_double(state) (mrg63k3a_ulong(state)*MRG63K3A_DOUBLE_MULTI)
+	private:
+		state_accessor  stateBuf;
+		input_accessor  seedArr;
+};
 
-/**
-Generates a random double using mrg63k3a RNG. Since mrg63k3a returns 64-bit numbers this is equivalent to mrg63k3a_double.
+// Kernel function
+// Generate random uint
+class mrg63k3a_rng_kernel{
+	public:
+	    using state_accessor =
+		    sycl::accessor<mrg63k3a_state, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+	    using output_accessor =
+		    sycl::accessor<dataT, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+		mrg63k3a_rng_kernel(int count,
+			state_accessor statePtr,
+			output_accessor dstPtr)
+		: num(count),
+		  stateBuf(statePtr),
+		  res(dstPtr) {}
+		void operator()(sycl::nd_item<1> item) {
+            uint gid=get_global_linear_id();
+            uint gsize=get_num_range(0);
+            mrg63k3a_state state;
+            state = stateBuf[gid];
+            for(uint i=gid;i<num;i+=gsize) {
+                res[i]= mrg63k3a_uint(state);
+            }
+            stateBuf[gid] = state;
+		}
 
-@param state State of the RNG to use.
-*/
-#define mrg63k3a_double2(state) mrg63k3a_double(state)
-)EOK";
+	private:
+		int             num;
+		state_accessor  stateBuf;
+		output_accessor res;
+};
+
+// Class function
+// Launch kernel to seed RNG by single ulong
+void MRG63K3A_PRNG::seed_by_value(sycl::queue funcQueue,
+				               size_t gsize,
+				               size_t lsize) {
+
+    funcQueue.submit([&] (sycl::handler& cgh) {
+        auto state_acc = this->stateBuf->template get_access<sycl::access::mode::read_write>(cgh);
+		// If seed by value, we will use the first element in seedArr as the value
+		seedVal = this->seedArr.data()[0];
+
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(gsize),
+		                                   sycl::range<1>(lsize)),
+		                 mrg63k3a_seed_by_value_kernel(seedVal, state_acc));
+    });
+}
+
+// Class function
+// Launch kernel to seed RNG by an array of ulong
+void MRG63K3A_PRNG::seed_by_array(sycl::queue funcQueue,
+				 size_t gsize,
+				 size_t lsize) {
+
+    funcQueue.submit([&] (sycl::handler& cgh) {
+        auto state_acc = this->stateBuf->template get_access<sycl::access::mode::read_write>(cgh);
+		this->seedBuf = cl::sycl::buffer<ulong, 1>(&this->seedArr, cl::sycl::range<1>(this->seedArr.size()));
+        auto seed_acc = this->seedBuf->template get_access<sycl::access::mode::read>(cgh);
+
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(gsize),
+		                                   sycl::range<1>(lsize)),
+		                 mrg63k3a_seed_by_array_kernel(seed_acc, state_acc));
+    });
+}
+
+void MRG63K3A_PRNG::generate_uint(sycl::queue funcQueue,
+				int count,
+                sycl::buffer<uint, 1> &dst,
+				size_t gsize,
+				size_t lsize) {
+
+    funcQueue.submit([&] (sycl::handler& cgh) {
+        auto state_acc = stateBuf->template get_access<sycl::access::mode::read_write>(cgh);
+        auto dst_acc = dst->template get_access<sycl::access::mode::read>(cgh);
+
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(gsize),
+		                                   sycl::range<1>(lsize)),
+		                 mrg63k3a_rng_kernel(count, state_acc, dst_acc));
+    });
+}
+
+#endif // __MRG63K3A_RNG__
