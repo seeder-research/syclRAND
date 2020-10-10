97d8522bdd937d97a6532f2a6a255f95bd625622
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2020-10-10 18:57:17 +0800

    Added ran2 prng

--- src/generators/ran2.hpp
+++ src/generators/ran2.hpp
@@ -1,13 +1,10 @@
-#define   NTAB 32
+#ifndef __RAN2_RNG__
+#define __RAN2_RNG__
 
-typedef struct{
-        int idum;
-        int idum2;
-        int iy;
-        int iv[NTAB];
-} ran2_state;
+#ifndef __SYCLRAND_BASE_CLASS
+#include "common/syclrand_def.hpp"
+#endif // __SYCLRAND_BASE_CLASS
 
-const char * ran2_prng_kernel = R"EOK(
 /**
 @file
 
@@ -83,6 +80,7 @@ ulong _ran2_uint(ran2_state* state){
 		return temp;
 	}*/
 }
+
 /**
 Generates a random 64-bit unsigned integer using ran2 RNG. The highest bit is always 0.
 
@@ -115,31 +113,138 @@ void ran2_seed(ran2_state* state, ulong seed){
 	state->iy = state->iv[0];
 }
 
-/**
-Generates a random 64-bit unsigned integer using ran2 RNG.
+// Kernel function
+// Seed RNG by single ulong
+class ran2_seed_by_value_kernel {
+	public:
+	    using state_accessor =
+		    sycl::accessor<ran2_state, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+		ran2_seed_by_value_kernel(ulong val,
+			state_accessor statePtr)
+		: seedVal(val),
+		  stateBuf(statePtr) {}
+		void operator()(sycl::nd_item<1> item) {
+            uint gid=get_global_linear_id(0);
+            ulong seed = (ulong)(gid);
+            seed <<= 1;
+            seed += seedVal;
+            if (seed == 0) {
+                seed += 1;
+            }
+            ran2_state state;
+            ran2_seed(&state, seed);
+            stateBuf[gid] = state;
+		}
 
-@param state State of the RNG to use.
-*/
-#define ran2_ulong(state) ((((ulong)ran2_uint(state)) << 32) | ran2_uint(state))
+	private:
+		ulong           seedVal;
+		state_accessor  stateBuf;
+};
 
-/**
-Generates a random float using ran2 RNG.
+// Kernel function
+// Seed RNG by array of ulong
+class ran2_seed_by_array_kernel {
+	public:
+	    using state_accessor =
+		    sycl::accessor<ran2_state, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+	    using input_accessor =
+		    sycl::accessor<ulong, 1, sycl::access::mode::read, sycl::access::target::global_buffer>;
+		ran2_seed_by_array_kernel(input_accessor seedArr,
+			state_accessor statePtr)
+		: seedArr(seedArr),
+		  stateBuf(statePtr) {}
+		void operator()(sycl::nd_item<1> item) {
+            uint gid=get_global_id(0);
+            ulong seed = seedArr[gid];
+            ran2_state state;
+            ran2_seed(&state,seed);
+            stateBuf[gid] = state;
+		}
 
-@param state State of the RNG to use.
-*/
-#define ran2_float(state) (ran2_uint(state)*RAN2_FLOAT_MULTI)
+	private:
+		state_accessor  stateBuf;
+		input_accessor  seedArr;
+};
 
-/**
-Generates a random double using ran2 RNG.
+// Kernel function
+// Generate random uint
+class ran2_rng_kernel{
+	public:
+	    using state_accessor =
+		    sycl::accessor<ran2_state, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+	    using output_accessor =
+		    sycl::accessor<dataT, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer>;
+		ran2_rng_kernel(int count,
+			state_accessor statePtr,
+			output_accessor dstPtr)
+		: num(count),
+		  stateBuf(statePtr),
+		  res(dstPtr) {}
+		void operator()(sycl::nd_item<1> item) {
+            uint gid=get_global_linear_id();
+            uint gsize=get_num_range(0);
+            ran2_state state;
+            state = stateBuf[gid];
+            for(uint i=gid;i<num;i+=gsize) {
+                res[i]= ran2_uint(state);
+            }
+            stateBuf[gid] = state;
+		}
 
-@param state State of the RNG to use.
-*/
-#define ran2_double(state) (ran2_ulong(state)*RAN2_DOUBLE2_MULTI + ran2_ulong(state)*RAN2_DOUBLE_MULTI)
+	private:
+		int             num;
+		state_accessor  stateBuf;
+		output_accessor res;
+};
 
-/**
-Generates a random double using ran2 RNG. Generated using only 32 random bits.
+// Class function
+// Launch kernel to seed RNG by single ulong
+void RAN2_PRNG::seed_by_value(sycl::queue funcQueue,
+				               size_t gsize,
+				               size_t lsize) {
 
-@param state State of the RNG to use.
-*/
-#define ran2_double2(state) (ran2_uint(state)*RAN2_DOUBLE2_MULTI)
-)EOK";
+    funcQueue.submit([&] (sycl::handler& cgh) {
+        auto state_acc = this->stateBuf->template get_access<sycl::access::mode::read_write>(cgh);
+		// If seed by value, we will use the first element in seedArr as the value
+		seedVal = this->seedArr.data()[0];
+
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(gsize),
+		                                   sycl::range<1>(lsize)),
+		                 ran2_seed_by_value_kernel(seedVal, state_acc));
+    });
+}
+
+// Class function
+// Launch kernel to seed RNG by an array of ulong
+void RAN2_PRNG::seed_by_array(sycl::queue funcQueue,
+				 size_t gsize,
+				 size_t lsize) {
+
+    funcQueue.submit([&] (sycl::handler& cgh) {
+        auto state_acc = this->stateBuf->template get_access<sycl::access::mode::read_write>(cgh);
+		this->seedBuf = cl::sycl::buffer<ulong, 1>(&this->seedArr, cl::sycl::range<1>(this->seedArr.size()));
+        auto seed_acc = this->seedBuf->template get_access<sycl::access::mode::read>(cgh);
+
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(gsize),
+		                                   sycl::range<1>(lsize)),
+		                 ran2_seed_by_array_kernel(seed_acc, state_acc));
+    });
+}
+
+void RAN2_PRNG::generate_uint(sycl::queue funcQueue,
+				int count,
+                sycl::buffer<uint, 1> &dst,
+				size_t gsize,
+				size_t lsize) {
+
+    funcQueue.submit([&] (sycl::handler& cgh) {
+        auto state_acc = stateBuf->template get_access<sycl::access::mode::read_write>(cgh);
+        auto dst_acc = dst->template get_access<sycl::access::mode::read>(cgh);
+
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(gsize),
+		                                   sycl::range<1>(lsize)),
+		                 ran2_rng_kernel(count, state_acc, dst_acc));
+    });
+}
+
+#endif // __RAN2_RNG__
